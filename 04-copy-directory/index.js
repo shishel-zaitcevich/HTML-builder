f1f7e8b98ecc456af335ad773e7ae35fbb12d022
shishel-zaitcevich <ushka_ya@mail.ru>
2022-05-23 17:21:56 +0300

    feat: 04 solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+// path.join: files, files-copy
+// delete files-copy folder ? (rm {force: true, recursive: true})
+// create .. (mkdir {recursive: true});
+// func copyDir() {}
+// copyDir();
 
+const fs = require('fs');
+const path = require('path');
+const files = path.join(__dirname, 'files');
+const files_copy = path.join(__dirname, 'files-copy');
+
+function createFolder (){
+  if(files_copy === true){
+    fs.rm(files_copy, {force: true, recursive: true}, (err) => {
+      if(err){ console.error(err);}
+    });
+  } else {
+    fs.mkdir(files_copy, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+  }
+}
+
+function copyDir (){
+fs.cp(files, files_copy, {recursive: true}, err => {
+  if(err) throw err; 
+});
+}
+
+createFolder();
+copyDir();
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,4 @@
+// delete bundle.css (rm) ?
+// styles = '' or []
+// readdir -> isFile && css { readFile; bundle appendFile or styles += fileContent}
+// styles -> bundle.css
